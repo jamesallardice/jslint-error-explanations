f0188770ba031f396d9379147de01891707651f8
James Allardice <jim.allardice@gmail.com>
2012-07-21 14:24:09 +0100

    Added search functionality to homepage

---
+++ assets/js/search.js
@@ -0,0 +1,39 @@
+$(function () {
+
+	"use strict";
+
+	var input = $("#filter"),
+		postColumns = $(".posts"),
+		posts = postColumns.find("li");
+
+	input.on("keyup", function () {
+
+		var search = this.value.toLowerCase(),
+			newColumn1 = $("<ul class='posts dynamic' />"),
+			newColumn2 = newColumn1.clone(),
+			perColumn,
+			visible;
+
+		$(".posts.dynamic").remove();
+		postColumns.hide();
+
+		visible = posts.filter(function () {
+			return $(this).find("h3").text().indexOf(search) > -1;
+		});
+
+		perColumn = Math.ceil(visible.length / 2);
+
+		postColumns.eq(0).before(newColumn1);
+		postColumns.eq(1).before(newColumn2);
+
+		visible.each(function (i) {
+			if (i < perColumn) {
+				newColumn1.append(visible.eq(i).clone().show());
+			} else {
+				newColumn2.append(visible.eq(i).clone().show());
+			}
+		});
+
+	});
+
+});
