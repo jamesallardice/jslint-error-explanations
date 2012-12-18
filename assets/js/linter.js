e61b8c277051178417040f5f4637c0354a296e7c
James Allardice <jim.allardice@gmail.com>
2012-12-18 16:02:48 +0000

    Initial work on JSLint/Hint version switching

--- assets/js/linter.js
+++ assets/js/linter.js
@@ -27,26 +27,49 @@ function runLint(editor, tool) {
 }
 
 var codeMirrorOpts = {
-	indentWithTabs: true,
-	indentUnit: 4,
-	lineNumbers: true,
-	fixedGutter: true,
-	lineWrapping: true,
-	onKeyEvent: function (editor) {
-		runLint(editor, $(editor.getInputField()).closest(".jslint").find(".jslint-choice-lint").hasClass("btn-inverse") ? "JSHint" : "JSLint");
-	}
-};
+		indentWithTabs: true,
+		indentUnit: 4,
+		lineNumbers: true,
+		fixedGutter: true,
+		lineWrapping: true,
+		onKeyEvent: function (editor) {
+			runLint(editor, $(editor.getInputField()).closest(".jslint").find(".jslint-choice-lint").hasClass("btn-inverse") ? "JSHint" : "JSLint");
+		}
+	},
+	jslintSelection = [
+		"<div class='btn-group' style='float: left'>",
+			"<button class='btn btn-mini dropdown-toggle jslint-choice-lint' data-toggle='dropdown'>",
+				"JSLint ",
+				"<span class='caret'></span>",
+			"</button>",
+			"<ul class='dropdown-menu'>",
+				"<li></li>",
+			"</ul>",
+		"</div>"
+	].join(""),
+	jshintSelection = [
+		"<div class='btn-group' style='float: left'>",
+			"<button class='btn btn-mini dropdown-toggle jslint-choice-hint' data-toggle='dropdown'>",
+				"JSHint ",
+				"<span class='caret'></span>",
+			"</button>",
+			"<ul class='dropdown-menu'>",
+				"<li></li>",
+			"</ul>",
+		"</div>"
+	].join("");
 
 $(function () {
 	$("textarea").each(function () {
 		var $this = $(this),
 			linter = $this.data("linter") || "JSLint";
 		$this.wrap("<div class='row'><div class='jslint span12'><div class='row'><div class='span7'></div></div></div></div>");
-		$this.closest(".row").append("<div class='jslint-output span5'><div class='jslint-result'><span class='result-output'></span><span class='jslint-choice pull-right'><button class='btn btn-mini jslint-choice-lint'>JSLint</button> <button class='btn btn-mini jslint-choice-hint'>JSHint</button></div><div class='jslint-messages'></div></div>").find(linter === "JSLint" ? ".jslint-choice-hint" : ".jslint-choice-lint").addClass("btn-inverse");
+		$this.closest(".row").append("<div class='jslint-output span5'><div class='jslint-result'><span class='result-output'></span><div class='jslint-choice pull-right'>" + jslintSelection + " " + jshintSelection + "</div></div><div class='jslint-messages'></div></div>").find(linter === "JSLint" ? ".jslint-choice-hint" : ".jslint-choice-lint").addClass("btn-inverse");
 		var mirror = CodeMirror.fromTextArea(this, codeMirrorOpts);
 		$this.data("cmInstance", mirror);
 		runLint(mirror, linter);
 	});
+	$(".dropdown-toggle").dropdown();
 	$("body").on("click", ".jslint-choice-lint", function () {
 		var $this = $(this);
 		$this.removeClass("btn-inverse").next().addClass("btn-inverse");
