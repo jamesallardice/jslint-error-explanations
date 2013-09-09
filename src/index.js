d41be5b6308671f9b5787a8dc2494299aba9ac1b
Tim Ruffles <timruffles@googlemail.com>
2013-09-09 15:27:58 +0000

    neaten code

--- src/index.js
+++ src/index.js
@@ -2,26 +2,25 @@ var config = require("../config.js");
 var express = require("express");
 var sh = require("shelljs");
 var _ = require("underscore");
-var cheerio = require("cheerio");
+var formatting = require("./formatting.js");
 
 
+// load content
 var Posts = require("./posts.js");
 var storage = require("./file_storage.js");
-
-var app = express();
 
 var posts = new Posts({
   items: sh.ls(__dirname + "/../content/explain/*.html").map(storage.readPost)
 });
+var pages = sh.ls(__dirname + "/../content/pages/*.html").map(storage.readPage);
 
-app.use('/assets',express.static(__dirname + '/../assets'))
 
+// view options
 var templates = require("./templating.js");
 var template = templates.fromPaths({
   partials: __dirname + "/../views/partials/*.html",
   layouts: __dirname + "/../views/layouts/*.html",
 });
-
 
 var contentConfig = require(__dirname + "/../config.yml");
 var authors = contentConfig.authors;
@@ -31,46 +30,16 @@ var defaultOpts = {
   debugJs: false
 };
 
-var toColumns = function(posts) {
-  var mid = Math.ceil(posts.length / 2);
-  return [{posts:posts.slice(0,mid)}, {posts:posts.slice(mid)}];
-};
-var formatTags = function(tags) {
-  var names = {
-    jshint: "JSHint",
-    jslint: "JSLint",
-    adsafe: "ADsafe"
-  }
-  return tags.map(function(tag) {
-    return {
-      name: names[tag],
-      tag: tag
-    }
-  })
-};
 
-var truncateWords = function(str) {
-  var words = str.split(" ")
-  if(words.length <= 30) return str;
-  return words.slice(0,30).join(" ").replace(/\.$/,"") + "…";
-}
+// setup routing
+var app = express();
+app.use('/assets',express.static(__dirname + '/../assets'))
 
-var formatPosts = function(posts) {
-  return posts.map(function(post) {
-    return _.defaults({
-      tags: formatTags(post.tags),
-      url: "/" + post.slug,
-      body: truncateWords(cheerio.load(post.body)("p").eq(1).text())
-    },post);
-  });
-}
-
-var pages = sh.ls(__dirname + "/../content/pages/*.html").map(storage.readPage);
 pages.forEach(function(page) {
   app.get("/" + page.slug,function(req,res) {
     var opts = _.extend({},page);
     if(page.posts) {
-      opts.columns = toColumns(formatPosts(posts.byTag(page.posts)))
+      opts.columns = formatting.toColumns(formatting.formatPosts(posts.byTag(page.posts)))
     }
     opts = _.extend(opts,defaultOpts);
     res.send(template.render(page.body,opts,{layout: page.layout}));
@@ -100,5 +69,6 @@ app.get("/:slug",function(req,res) {
 });
 
 
+// boot app
 console.log("lint-explain listening on " + config.port);
 app.listen(config.port);
